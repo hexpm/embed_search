d882dfc8699115e36be1d144133d92322cc502ee
Karol Wilk <karol.t.wilk@gmail.com>
2024-03-06 16:31:32 +0100

    Clean up import/alias in EmbeddingTest

--- test/search/embedding_test.exs
+++ test/search/embedding_test.exs
@@ -1,18 +1,17 @@
 defmodule Search.EmbeddingTest do
   use ExUnit.Case, async: true
 
-  require Nx
   alias Search.Embedding
 
   test "creates embedding tensor of correct shape for a single input" do
-    %{embedding: embedding} = Nx.Serving.batched_run(Search.Embedding, "The cat chases the mouse")
+    %{embedding: embedding} = Nx.Serving.batched_run(Embedding, "The cat chases the mouse")
 
     assert Nx.shape(embedding) == {Embedding.embedding_size()}
   end
 
   test "creates embedding tensor of correct shape for batched inputs" do
     [%{embedding: embedding1}, %{embedding: embedding2}] =
-      Nx.Serving.batched_run(Search.Embedding, [
+      Nx.Serving.batched_run(Embedding, [
         "The cat chases the mouse",
         "Lorem ipsum dolor sit amet"
       ])

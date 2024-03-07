582fa02721ac7bbaaec3310d02546ffa719ed4f2
Karol Wilk <karol.t.wilk@gmail.com>
2024-03-07 07:14:26 +0100

    Add non null constraint to fragments

--- priv/repo/migrations/20240226120519_create_fragments.exs
+++ priv/repo/migrations/20240226120519_create_fragments.exs
@@ -3,8 +3,8 @@ defmodule Search.Repo.Migrations.CreateFragments do
 
   def change do
     create table(:fragments) do
-      add :doc_text, :text
-      add :embedding, :vector, size: Search.Embedding.embedding_size()
+      add :doc_text, :text, null: false
+      add :embedding, :vector, size: Search.Embedding.embedding_size(), null: false
 
       timestamps(type: :utc_datetime)
     end

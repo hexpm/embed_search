9b9b742a3adad352865c5ef742cefff9e5c7b419
Karol Wilk <karol.t.wilk@gmail.com>
2024-03-07 07:16:06 +0100

    Inline unnecessary private function

--- lib/search/hex_client.ex
+++ lib/search/hex_client.ex
@@ -7,7 +7,16 @@ defmodule Search.HexClient do
   def get_releases(package_name) when is_binary(package_name) do
     case get("#{@api_url}/packages/#{package_name}") do
       {:ok, %{status: 200, body: %{"releases" => releases}}} ->
-        map_json_to_releases(package_name, releases)
+        res =
+          for %{"has_docs" => has_docs, "version" => version} <- releases do
+            %HexClient.Release{
+              package_name: package_name,
+              version: Version.parse!(version),
+              has_docs: has_docs
+            }
+          end
+
+        {:ok, res}
 
       {:ok, %{status: status}} ->
         {:error, "HTTP #{status}"}
@@ -15,29 +24,6 @@ defmodule Search.HexClient do
       err ->
         err
     end
-  end
-
-  defp map_json_to_releases(package_name, releases_json) do
-    map_json_to_releases(package_name, releases_json, [])
-  end
-
-  defp map_json_to_releases(_package_name, [] = _releases_json, acc) do
-    {:ok, Enum.reverse(acc)}
-  end
-
-  defp map_json_to_releases(
-         package_name,
-         [%{"has_docs" => has_docs, "version" => version} | tail] = _releases_json,
-         acc
-       ) do
-    release = %HexClient.Release{
-      package_name: package_name,
-      version: Version.parse!(version),
-      has_docs: has_docs
-    }
-
-    acc = [release | acc]
-    map_json_to_releases(package_name, tail, acc)
   end
 
   def get_docs_tarball(

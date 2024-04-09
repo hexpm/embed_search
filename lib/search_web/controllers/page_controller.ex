ac96408d4ed78a27d028a4330604db3d3cdc0b36
Karol Wilk <karol.t.wilk@gmail.com>
2024-04-09 13:03:34 +0200

    Update the frontend to work with new changes

--- lib/search_web/controllers/page_controller.ex
+++ lib/search_web/controllers/page_controller.ex
@@ -1,13 +1,30 @@
 defmodule SearchWeb.PageController do
   use SearchWeb, :controller
 
+  @empty_form Phoenix.Component.to_form(%{
+                "search_text" => nil,
+                "k" => nil,
+                "embedding_model" => nil
+              })
+
+  @embedding_model_opts Search.Application.embedding_models()
+                        |> Enum.map(&(&1 |> Module.split() |> Enum.reverse() |> Enum.at(0)))
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, form: Phoenix.Component.to_form(%{"search_text" => nil, "k" => nil}))
+    render(conn, :home, form: @empty_form, embedding_models: @embedding_model_opts)
   end
 
-  def search(conn, %{"k" => k, "search_text" => search_text} = params) do
+  def search(
+        conn,
+        %{"k" => k, "search_text" => search_text, "embedding_model" => embedding_model} = params
+      ) do
+    model =
+      Enum.find(Search.Application.embedding_models(), fn mod ->
+        "Elixir.Search.Embeddings.#{embedding_model}" == "#{mod}"
+      end)
+
     k = String.to_integer(k)
     search_text = String.trim(search_text)
 
@@ -19,10 +36,14 @@ defmodule SearchWeb.PageController do
       end
 
     if errors == [] do
-      %{embedding: query_tensor} = Nx.Serving.batched_run(Search.Embedding, search_text)
-      fragments = Search.Fragment.knn_lookup(query_tensor, k: k)
+      %{embedding: query_tensor} = Nx.Serving.batched_run(model, search_text)
 
-      render(conn, :search, fragments: fragments)
+      items =
+        Search.Embeddings.knn_query(model, query_tensor, k: k)
+        |> Stream.map(& &1.doc_fragment.doc_item)
+        |> Enum.uniq_by(& &1.id)
+
+      render(conn, :search, items: items)
     else
       render(conn, :home, form: Phoenix.Component.to_form(params, errors: errors))
     end

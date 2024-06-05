4f85ed479dc33c130b045ba20a994b4c30f945aa
Karol Wilk <karol.t.wilk@gmail.com>
2024-06-05 15:08:48 +0200

    Remove unnecessary docstrings

--- lib/search/packages.ex
+++ lib/search/packages.ex
@@ -1,8 +1,4 @@
 defmodule Search.Packages do
-  @moduledoc """
-  The Packages context.
-  """
-
   import Ecto.Query, warn: false
   alias Search.Repo
 
@@ -77,114 +73,40 @@ defmodule Search.Packages do
     :ok
   end
 
-  @doc """
-  Creates a doc fragment.
-  """
   def create_doc_fragment(%DocItem{id: item_id} = _doc_item, attrs) do
     %DocFragment{doc_item_id: item_id}
     |> DocFragment.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Creates a doc item.
-  """
   def create_doc_item(%Package{id: package_id} = _package, attrs) do
     %DocItem{package_id: package_id}
     |> DocItem.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Returns the list of packages.
-
-  ## Examples
-
-      iex> list_packages()
-      [%Package{}, ...]
-
-  """
   def list_packages do
     Repo.all(Package)
   end
 
-  @doc """
-  Gets a single package.
-
-  Raises `Ecto.NoResultsError` if the Package does not exist.
-
-  ## Examples
-
-      iex> get_package!(123)
-      %Package{}
-
-      iex> get_package!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_package!(id), do: Repo.get!(Package, id)
 
-  @doc """
-  Creates a package.
-
-  ## Examples
-
-      iex> create_package(%{field: value})
-      {:ok, %Package{}}
-
-      iex> create_package(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_package(attrs \\ %{}) do
     %Package{}
     |> Package.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a package.
-
-  ## Examples
-
-      iex> update_package(package, %{field: new_value})
-      {:ok, %Package{}}
-
-      iex> update_package(package, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_package(%Package{} = package, attrs) do
     package
     |> Package.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a package.
-
-  ## Examples
-
-      iex> delete_package(package)
-      {:ok, %Package{}}
-
-      iex> delete_package(package)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_package(%Package{} = package) do
     Repo.delete(package)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking package changes.
-
-  ## Examples
-
-      iex> change_package(package)
-      %Ecto.Changeset{data: %Package{}}
-
-  """
   def change_package(%Package{} = package, attrs \\ %{}) do
     Package.changeset(package, attrs)
   end

690443f7828a137a6cdf186c2f62ddf4b9e81503
Karol Wilk <karol.t.wilk@gmail.com>
2024-08-05 11:54:43 +0200

    Update search.add mix task to work with fragmentation

--- lib/mix/tasks/search.add.ex
+++ lib/mix/tasks/search.add.ex
@@ -3,7 +3,7 @@ defmodule Mix.Tasks.Search.Add do
   alias Search.HexClient
 
   @moduledoc """
-  Usage: mix #{Mix.Task.task_name(__MODULE__)} <PACKAGE> [<VERSION>]
+  Usage: mix #{Mix.Task.task_name(__MODULE__)} <PACKAGE> [version:<VERSION>] [max_size:<MAX_SIZE>]
 
   Fetches the documentation for the given package from Hex. Does not embed it yet.
 
@@ -17,21 +17,44 @@ defmodule Mix.Tasks.Search.Add do
 
   @impl Mix.Task
   def run(args) do
-    [package | args_tail] = args
+    [package_name | args_tail] = args
 
-    package_or_release =
-      case args_tail do
-        [version] ->
-          version = Version.parse!(version)
-          %HexClient.Release{package_name: package, version: version}
-
-        [] ->
-          package
-      end
-
-    case Packages.add_package(package_or_release) do
-      {:ok, package} -> Mix.shell().info("Package #{package.name}@#{package.version} added.")
+    with {:ok, args} <- parse_args(args_tail, []),
+         version = Keyword.get(args, :version),
+         fragmentation_opts = Keyword.take(args, [:max_size]),
+         package_or_release = package_or_release(package_name, version),
+         {:ok, package} <-
+           Packages.add_package(package_or_release, fragmentation_opts: fragmentation_opts) do
+      Mix.shell().info("Package #{package.name}@#{package.version} added.")
+    else
       {:error, err} -> Mix.shell().error("Error: #{err}")
+    end
+  end
+
+  defp package_or_release(package_name, nil), do: package_name
+
+  defp package_or_release(package_name, version) do
+    %HexClient.Release{package_name: package_name, version: version}
+  end
+
+  defp parse_args([], acc), do: {:ok, acc}
+
+  defp parse_args([arg | args_tail], acc) do
+    case arg do
+      "version:" <> version ->
+        case Version.parse(version) do
+          {:ok, version} -> parse_args(args_tail, Keyword.put(acc, :version, version))
+          :error -> {:error, ~c(Could not parse the "version" arg value)}
+        end
+
+      "max_size:" <> max_size ->
+        case Integer.parse(max_size) do
+          {max_size, ""} -> parse_args(args_tail, Keyword.put(acc, :max_size, max_size))
+          _ -> {:error, ~c(Could not parse the "max_size" arg value)}
+        end
+
+      _ ->
+        {:error, ~c(Unknown argument: "#{arg}")}
     end
   end
 end
--- lib/search/packages.ex
+++ lib/search/packages.ex
@@ -1,5 +1,6 @@
 defmodule Search.Packages do
   import Ecto.Query, warn: false
+  alias Search.FragmentationScheme
   alias Search.Repo
 
   alias Search.Packages.{Package, DocItem, DocFragment}
@@ -11,19 +12,27 @@ defmodule Search.Packages do
   If given a package name, adds the latest version of the package to the app. If given a `%HexClient.Release{}` adds
   the specified release. Does not embed it yet.
   """
-  def add_package(package_name) when is_binary(package_name) do
+  def add_package(name_or_release, opts \\ [])
+
+  def add_package(package_name, opts) when is_binary(package_name) do
     case HexClient.get_releases(package_name) do
       {:ok, releases} ->
+
         latest = HexClient.Release.latest(releases)
-        add_package(latest)
+        add_package(latest, opts)
 
       err ->
         err
     end
   end
 
-  def add_package(%HexClient.Release{package_name: package_name, version: version} = release) do
+  def add_package(
+        %HexClient.Release{package_name: package_name, version: version} = release,
+        opts
+      ) do
     version = Version.to_string(version)
+
+    fragmentation_opts = Keyword.get(opts, :fragmentation_opts, [])
 
     with {:ok, docs} <- HexClient.get_docs_tarball(release),
          {:ok, search_data} <- ExDocParser.extract_search_data(docs) do
@@ -43,25 +52,46 @@ defmodule Search.Packages do
           |> Ecto.Changeset.put_assoc(:doc_items, [])
 
         with {:ok, package} <- Repo.insert_or_update(package),
-             :ok <- create_items_from_package(package, search_data) do
+             :ok <- create_items_from_package(package, search_data, fragmentation_opts) do
           {:ok, package}
         end
       end)
     end
   end
 
-  defp create_items_from_package(%Package{} = _package, []), do: :ok
+  defp create_items_from_package(%Package{} = _package, [], _fragmentation_opts), do: :ok
 
-  defp create_items_from_package(%Package{} = package, [search_data_head | search_data_tail]) do
+  defp create_items_from_package(
+         %Package{} = package,
+         [search_data_head | search_data_tail],
+         fragmentation_opts
+       ) do
     %{"doc" => doc, "title" => title, "ref" => ref, "type" => type} = search_data_head
 
     with {:ok, item} <-
-           create_doc_item(package, %{doc: doc, title: title, ref: ref, type: type}),
-         {:ok, _fragment} <-
-           create_doc_fragment(item, %{
-             text: "# #{title}\n\n#{doc}"
-           }) do
-      create_items_from_package(package, search_data_tail)
+           create_doc_item(package, %{title: title, ref: ref, type: type}),
+         fragments =
+           doc
+           |> FragmentationScheme.split(fragmentation_opts)
+           |> Enum.with_index(),
+         {:ok, _fragments} <-
+           create_doc_fragments_from_binaries(item, fragments, []) do
+      create_items_from_package(package, search_data_tail, fragmentation_opts)
+    end
+  end
+
+  defp create_doc_fragments_from_binaries(_doc_item, [], acc), do: {:ok, acc}
+
+  defp create_doc_fragments_from_binaries(doc_item, [{text, order} | texts_tail], acc) do
+    case create_doc_fragment(doc_item, %{
+           text: text,
+           order: order
+         }) do
+      {:ok, fragment} ->
+        create_doc_fragments_from_binaries(doc_item, texts_tail, [fragment | acc])
+
+      {:error, _} = err ->
+        err
     end
   end
 
